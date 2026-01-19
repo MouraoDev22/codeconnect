2c05a71e463e5e3d1761d6f39a727798b491fe01
Mourão <p3dr0sk0sk7160@gmail.com>
2026-01-19 02:45:58 -0300

    adiciona funcionalidade para gerenciar tags e carregar imagem após upload

--- script.js
+++ script.js
@@ -1,30 +1,18 @@
+const tagsDisponiveis = ["Front-end", "Back-end", "Full-stack", "HTML", "CSS", "JavaScript", "React", "Vue", "Angular", "Node.js", "Data Science"]
+
 const uploadBtn = document.getElementById("upload-btn");
 const inputUpload = document.getElementById("imagem-upload");
+const imagemPrincipal = document.querySelector(".main-imagem");
+const nomeDaImagem = document.querySelector(".container-imagem-nome p");
+const inputTags = document.getElementById("input-tags");
+const listaTags = document.getElementById("lista-tags");
 
-uploadBtn.addEventListener("click", () => {
+uploadBtn.addEventListener("click", function(e) {
     inputUpload.click();
 });
 
-function lerConteudoDoArquivo(arquivo) {
-    return new Promise((resolve, reject) => {
-        const leitor = new FileReader();
-        leitor.onload = () => {
-            resolve({ url: leitor.result, nome: arquivo.name });
-        }
-    
-        leitor.onerror = () => {
-            reject(new Error(`Erro na leitura do arquivo ${arquivo.name}`));
-        }
-
-        leitor.readAsDataURL(arquivo);
-    });
-}
-
-const imagemPrincipal = document.querySelector(".main-imagem");
-const nomeDaImagem = document.querySelector(".container-imagem-nome p");
-
-inputUpload.addEventListener("change", async (evento) => {
-    const arquivo = evento.target.files[0];
+inputUpload.addEventListener("change", async function(e) {
+    const arquivo = e.target.files[0];
 
     if (arquivo) {
         try {
@@ -36,3 +24,46 @@ inputUpload.addEventListener("change", async (evento) => {
         }
     }
 });
+
+inputTags.addEventListener("keypress", function(e) {
+    if (e.key === "Enter") {
+        e.preventDefault();
+        const tagTexto = inputTags.value.trim();
+        if (tagTexto !== "") {
+            const novaTag = document.createElement("li");
+            novaTag.innerHTML = `<p>${tagTexto}</p> <img src="./img/close-black.svg" class="remove-tag">`;
+            listaTags.appendChild(novaTag);
+            inputTags.value = "";
+        }
+    }
+});
+
+listaTags.addEventListener("click", function(e) {
+    if (e.target.classList.contains("remove-tag")) {
+        const tagQueSeraRemovida = e.target.parentElement;
+        listaTags.removeChild(tagQueSeraRemovida);
+    }
+});
+
+function lerConteudoDoArquivo(arquivo) {
+    return new Promise((resolve, reject) => {
+        const leitor = new FileReader();
+        leitor.onload = function() {
+            resolve({ url: leitor.result, nome: arquivo.name });
+        }
+    
+        leitor.onerror = function() {
+            reject(new Error(`Erro na leitura do arquivo ${arquivo.name}`));
+        }
+
+        leitor.readAsDataURL(arquivo);
+    });
+}
+
+async function verificaTagsDisponiveis(tagTexto) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(tagsDisponiveis.includes(tagTexto));
+        }, 1000);
+    });
+}
